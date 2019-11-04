dbf7d90d0ab449b3b150c9c42d04558e4a67f6ba
Danielle Rubin <dlhrubin@gmail.com>
2019-11-04 15:48:58 -0500

    Implement deleting albums from selection box

--- src/components/AlbumSelect.js
+++ src/components/AlbumSelect.js
@@ -7,7 +7,7 @@ export class AlbumSelect extends Component {
             newArtist: "",
             newAlbum: "",
             searchVis: "hidden",
-            selections: []
+            selections: [],
         }
         this.artistInput = React.createRef();
         this.albumInput = React.createRef();        
@@ -42,12 +42,18 @@ export class AlbumSelect extends Component {
         }
     }
 
+    unSelect = (artist, album) => {
+        this.setState({
+            selections: this.state.selections.filter(selection => (selection.artist !== artist && selection.album !== album))
+        })
+    }
+
     render() {
         let selectedAlbums = this.state.selections.map((selection, i) => {
             return (
                 <div key={i}>
                     <span>{selection.album + ", " + selection.artist}</span>
-                    <button>
+                    <button onClick={this.unSelect.bind(this, selection.artist, selection.album)}>
                         <i className="fas fa-times"></i>
                     </button>
                 </div>
@@ -57,18 +63,24 @@ export class AlbumSelect extends Component {
             <div className="album-options">
                 <h2>Albums:</h2>
                 <form onSubmit={this.handleSubmit.bind(this, true)}>
-                    Artist
-                    <input type="text" placeholder="Enter artist name..." disabled={(this.state.selections.length === 10) ? "disabled" : ""} ref={this.artistInput} value={this.state.newArtist} onChange={this.handleChange.bind(this, true)}></input>
-                    <button type="submit" disabled={(this.state.selections.length === 10) ? "disabled" : ""}>
-                        <i className="fas fa-search"></i>
-                    </button>                    
+                    <div>
+                        <span>Artist</span>
+                        <input type="text" placeholder="Enter artist name..." disabled={(this.state.selections.length === 10) ? "disabled" : ""} ref={this.artistInput} value={this.state.newArtist} onChange={this.handleChange.bind(this, true)}></input>
+                        <button type="submit" disabled={(this.state.selections.length === 10) ? "disabled" : ""}>
+                            <i className="fas fa-search"></i>
+                        </button>     
+                    </div>
+                    {/*NTS: may want to use a conditional render here instead, esp. after adding other error messages}*/}
+                    <p className="warning">{(this.state.selections.length === 10) ? "10 albums already selected" : ""}</p>               
                 </form>
                 <form onSubmit={this.handleSubmit.bind(this, false)} style={{visibility: this.state.searchVis}}>
-                    Album
-                    <input type="text" placeholder="Enter album name..." ref={this.albumInput} value={this.state.newAlbum} onChange={this.handleChange.bind(this, false)}></input>
-                    <button type="submit">
-                        <i className="fas fa-search"></i>
-                    </button>
+                    <div>
+                        <span>Album</span>
+                        <input type="text" placeholder="Enter album name..." ref={this.albumInput} value={this.state.newAlbum} onChange={this.handleChange.bind(this, false)}></input>
+                        <button type="submit">
+                            <i className="fas fa-search"></i>
+                        </button>
+                    </div>
                 </form>
                 <div className="album-selection">
                     <p>Selection (max of 10 albums)</p>
