59ba0e82d95f784b6bda74721847fe2f4588806c
Danielle Rubin <dlhrubin@gmail.com>
2019-11-04 18:29:12 -0500

    Implement API call from user input

--- src/components/AlbumSelect.js
+++ src/components/AlbumSelect.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import axios from "axios";
+import config from "../config"
 
 export class AlbumSelect extends Component {
     constructor(props) {
@@ -14,6 +16,7 @@ export class AlbumSelect extends Component {
         this.albumInput = React.createRef();        
     }
 
+    // Display warning that the max number of albums have been selected
     handleClick = () => {
         if (this.state.selections.length === 10) {
             this.setState({
@@ -27,6 +30,7 @@ export class AlbumSelect extends Component {
         }
     }
 
+    // Update local state values for new artist and album entries as user types
     handleChange = (isArtist, e) => {
         this.setState({
             newArtist: (isArtist) ? e.target.value : this.state.newArtist,
@@ -38,32 +42,50 @@ export class AlbumSelect extends Component {
 
     handleSubmit = (isArtist, e) => {
         e.preventDefault();
+        // Check that user has entered text into the field
         let hasContent = (isArtist) ? this.state.newArtist : this.state.newAlbum;
         if (hasContent) {
-            let notRepeat = (isArtist) ? true : (this.state.selections.filter(selection => (selection.artist === this.state.newArtist && selection.album === this.state.newAlbum)).length === 0)
-            if (notRepeat) {
+            if (isArtist) {
                 this.setState({
-                    newArtist: (isArtist) ? this.state.newArtist : "",
-                    newAlbum: (isArtist) ? this.state.newAlbum : "",
-                    searchVis: (isArtist) ? "visible" : "hidden",
-                    selections: (isArtist) ? this.state.selections : [...this.state.selections, {artist: this.state.newArtist, album: this.state.newAlbum}]
+                    searchVis: "visible" // Toggle album search field visibility
                 })
-                if (isArtist) {
-                    //Jump focus to the album search form after state changes to make it visible
-                    setTimeout(() => {
-                        this.albumInput.current.focus();
-                    }, 1)
-                } else {
-                    this.artistInput.current.focus();
-                }                
+                // Jump focus to the album search field after state changes to make it visible
+                setTimeout(() => {
+                    this.albumInput.current.focus();
+                }, 1)
             } else {
-                this.setState ({
-                    warnings: {artist: "", album: "Album already selected"}
+                axios.get("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" + config.API_KEY + "&artist=" + this.state.newArtist + "&album=" + this.state.newAlbum + "&format=json")
+                .then(res => {
+                    if (res.data.hasOwnProperty("album")) {
+                        // Check that the album entered has not already been selected
+                        let notRepeat = (this.state.selections.filter(selection => (selection.artist === res.data.album.artist && selection.album === res.data.album.name)).length === 0)
+                        if (notRepeat) {
+                            this.setState({
+                                newArtist: "",
+                                newAlbum: "",
+                                searchVis: "hidden", // Toggle album search field visibility
+                                selections: [...this.state.selections, {artist: res.data.album.artist, album: res.data.album.name}]
+                            //coverURL: res.data.album.image.filter((entry) => entry.size === "large")[0]["#text"]
+                            })
+                            // Jump focus to the artist search field after submitting new album
+                            this.artistInput.current.focus();
+                        } else {
+                            this.setState ({
+                                warnings: {artist: "", album: "Album already selected"}
+                            })
+                        }
+                    } else {
+                        this.setState({
+                            // Note: replacing warning under artist field with a space to trigger changing the border color to red around both fields
+                            warnings: {artist: " ", album: "Artist or album not found"}
+                        })
+                    }
                 })
             }
         }
     }
 
+    // Remove album from selection
     unSelect = (artist, album) => {
         this.setState({
             selections: this.state.selections.filter(selection => (selection.artist !== artist || selection.album !== album)),
@@ -73,6 +95,7 @@ export class AlbumSelect extends Component {
     }
 
     render() {
+        // Populate selection box with selected albums (artist name and album thumbnail)
         let selectedAlbums = this.state.selections.map((selection, i) => {
             return (
                 <div key={i}>
@@ -83,6 +106,7 @@ export class AlbumSelect extends Component {
                 </div>
             )
         })
+        // Add red border to search boxes when a warning is present
         let warningBorder = (field) => this.state.warnings[field] ? {borderColor: "red"} : {borderColor: ""};
         return (
             <div className="album-options">
@@ -95,7 +119,6 @@ export class AlbumSelect extends Component {
                             <i className="fas fa-search"></i>
                         </button>     
                     </div>
-                    {/*NTS: may want to use a conditional render here instead, esp. after adding other error messages}*/}
                     <p className="warning">{this.state.warnings.artist}</p>               
                 </form>
                 <form onSubmit={this.handleSubmit.bind(this, false)} style={{visibility: this.state.searchVis}}>
--- src/components/Collage.js
+++ src/components/Collage.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import axios from "axios";
-import config from "../config"
 
 export class Collage extends Component {
     constructor(props) {
@@ -11,18 +9,7 @@ export class Collage extends Component {
             coverURL: "https://lastfm.freetls.fastly.net/i/u/174s/83fce5201587652d2487123aeeb027b6.png"
         }
     }
-    /*
-    componentDidMount() {
-        axios.get("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" + config.API_KEY + "&artist=Hozier&album=Wasteland,+Baby!&format=json")
-            .then(res => {
-                this.setState({
-                    artist: res.data.album.artist,
-                    title: res.data.album.name,
-                    coverURL: res.data.album.image.filter((entry) => entry.size === "large")[0]["#text"]
-                })
-            })
-    }
-    */
+
     render() {
         return (
             <div className="collage">
