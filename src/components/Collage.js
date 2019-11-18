a341fa16cedb05a6d7be9e838687a5414bcad17f
Danielle Rubin <dlhrubin@gmail.com>
2019-11-17 21:02:41 -0500

    Implement collage overlay and shuffle function

--- src/components/Collage.js
+++ src/components/Collage.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react'
 
+// Shuffle album order
+let shuffle = (arr) => {
+    let unshuffled = [...arr];
+    let shuffled = [];
+    for (let i=0; i < arr.length; i++) {
+        shuffled = [...shuffled, unshuffled.splice(Math.floor(Math.random() * unshuffled.length), 1)[0]];
+    }
+    return shuffled
+}
+
 export class Collage extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
+        let collage;
+        let covers = shuffle(this.props.selections);
+        // Implement diamond collage shape
+        if (this.props.shape === "diamond") {
+            if (covers.length === 2) {
+                collage = covers.map((album, i) => {
+                    return (<div key={album.album}>
+                                <img src={album.cover} alt={album.album + ", " + album.artist}></img>
+                                <div className={"overlay " + ((i) ? "top-left" : "top-right")} ></div>
+                            </div>)
+                });
+                collage.reverse();
+                collage = [...collage, covers.map((album, i) => {
+                    return (<div key={album.album}>
+                                <img src={album.cover} alt={album.album + ", " + album.artist}></img>
+                                <div className={"overlay " + ((i) ? "bottom-right" : "bottom-left")}></div>
+                            </div>)
+                })];
+            }
+        } else if (this.props.shape === "square") {
+            if (covers.length === 4) {
+                collage = covers.map(album => {
+                    return (<div key={album.album}>
+                                <img src={album.cover} alt={album.album + ", " + album.artist}></img>
+                            </div>)
+                });
+            }
+        } else {
+            collage = <div></div>
+        }
         return (
-            <div className="collage">
-            </div>
+            <section className="collage">
+                <div className={"collage-grid " + this.props.shape + "-" + covers.length}>
+                    {collage}
+                </div>
+            </section>
         )
     }
 }
