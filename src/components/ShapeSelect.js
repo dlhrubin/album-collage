dc7888d039939cc0dd8614525ea229f3b2d7721f
Danielle Rubin <dlhrubin@gmail.com>
2019-12-04 14:56:21 -0500

    Change x, square, diamond, and heart icons to SVG

--- src/components/ShapeSelect.js
+++ src/components/ShapeSelect.js
@@ -9,48 +9,36 @@ export class ShapeSelect extends Component {
                     name: "cross",
                     active: false,
                     coords: "21,3 15,3 15,15 3,15 3,21 15,21 15,33 21,33 21,21 33,21 33,15 21,15",
-                    unclicked: "fas",
-                    clicked: "fas",
                     numbers: [5, 9, 13, 17, 20, 28]
                 },
                 {
                     name: "x",
                     active: false,
-                    icon: "fa-times",
-                    unclicked: "fas",
-                    clicked: "fas",
+                    coords: "29,3 18,14 7,3 3,7 14,18 3,29 7,33 18,22 29,33 33,29 22,18 33,7",
                     numbers: [10, 14, 18, 22, 26, 30]
                 },
                 {
                     name: "square",
                     active: false,
-                    icon: "fa-square", 
-                    unclicked: "fas",
-                    clicked: "fas",
+                    coords: "32,4 4,4 4,32 32,32", 
                     numbers: [4, 9, 16, 25]
                 },
                 {
                     name: "diamond",
                     active: false,
-                    icon: "fa-square",
-                    unclicked: "fas",
-                    clicked: "fas",
+                    coords: "18,3 3,18 18,33 33,18",
                     numbers: [2, 8, 18]
                 },
                 {
                     name: "heart",
                     active: false,
-                    icon: "fa-heart",
-                    unclicked: "fas",
-                    clicked: "fas",
+                    coords: "32,8 28,4 24,4 18,10 12,4 8,4 4,8 4,16 18,30 32,16",
                     numbers: [6, 10, 22, 24]
                 },
                 {
                     name: "octagon",
                     active: false,
                     coords: "23,3 13,3 3,13 3,23 13,33 23,33 33,23 33,13",
-                    unclicked: "fas",
-                    clicked: "fas",
                     numbers: [7, 28]
                 }
             ]
@@ -93,10 +81,9 @@ export class ShapeSelect extends Component {
 
     render() {
         const shapes = this.state.shapes.map((shape) => {
-            // Fill shape if shape has been selected and submit button hasn't been pressed
-            let shapeClass = (shape.active && this.props.shape) ? shape.clicked : shape.unclicked;
             let shapeStyle = {};
             let svgStyle = {};
+            // Fill shape if shape has been selected and submit button hasn't been pressed
             // Make shape orange if shape has been selected and submit button hasn't been pressed
             // Make shapes red on "no shape selected" submission error
             svgStyle["fill"] = svgStyle["stroke"] = (this.props.errors.shape) ? "red" : (shape.active && this.props.shape) ? "var(--highlight)" : "";
