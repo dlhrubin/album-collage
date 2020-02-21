4750f996884254a28c2902bbb41f2e20749e1b62
Danielle Rubin <dlhrubin@gmail.com>
2020-02-21 16:52:27 -0500

    Lint and refactor ShapeSelect

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import './App.scss';
+import Menu from './components/Menu';
+import Collage from './components/Collage';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: [],
+      selections: [],
+      shape: '',
+      editing: false,
+      hidePanel: false,
+    };
+    this.collageComponent = React.createRef();
+  }
+
+    // Determine whether only one panel (menu or collage) should be displayed at a time based on browser width
+    handleResize = () => {
+      // Width calculation adapted from jQuery source code
+      const windowWidth = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth);
+      if (windowWidth < 750 && !this.state.hidePanel) {
+        this.setState({
+          hidePanel: true,
+        });
+      } else if (windowWidth >= 750 && this.state.hidePanel) {
+        this.setState({
+          hidePanel: false,
+        });
+      }
+    }
+
+    componentDidMount() {
+      this.handleResize();
+      window.addEventListener('resize', this.handleResize);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.handleResize);
+    }
+
+    // Submit album selections and shape
+    handleSubmit = (selections, shape) => {
+      this.setState({
+        userInput: selections,
+        selections,
+        shape,
+        editing: false,
+      });
+      setTimeout(() => {
+        this.collageComponent.current.handleChangeFocus();
+      }, 1);
+    }
+
+    // Shuffle album order
+    handleShuffle = () => {
+      let sameAsBefore = true;
+      let unshuffled;
+      let shuffled;
+      // Check that new order is not identical to old order
+      while (sameAsBefore) {
+        unshuffled = [...this.state.selections];
+        shuffled = [];
+        for (let i = 0; i < this.state.selections.length; i++) {
+          shuffled = [...shuffled, unshuffled.splice(Math.floor(Math.random() * unshuffled.length), 1)[0]];
+          sameAsBefore = (sameAsBefore === false) ? false : (shuffled[i] === this.state.selections[i]);
+        }
+      }
+      this.setState({
+        selections: shuffled,
+      });
+    }
+
+    // Edit collage
+    handleEdit = () => {
+      this.setState({
+        editing: !this.state.editing,
+      });
+    }
+
+    // Reset collage to original, unshuffled user input album order
+    handleReset = () => {
+      this.setState({
+        selections: this.state.userInput,
+      });
+    }
+
+    // Delete collage
+    handleDelete = () => {
+      this.setState({
+        selections: [],
+        shape: '',
+      });
+    }
+
+    render() {
+      // On small screens, hide either selection menu or collage depending on user interaction
+      const panelToDisplay = (!this.state.hidePanel) ? '' : (this.state.selections.length && !this.state.editing) ? 'collage' : 'menu';
+
+      return (
+        <div className="app">
+          <Menu selections={this.state.selections} shape={this.state.shape} editing={this.state.editing} panelToDisplay={panelToDisplay} submitCollage={this.handleSubmit} editCollage={this.handleEdit} />
+          <Collage ref={this.collageComponent} selections={this.state.selections} shape={this.state.shape} editing={this.state.editing} userInput={this.state.userInput} panelToDisplay={panelToDisplay} shuffleCollage={this.handleShuffle} editCollage={this.handleEdit} resetCollage={this.handleReset} deleteCollage={this.handleDelete} />
+        </div>
+      );
+    }
+}
+
+export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 // Helper function to get computed style from DOM
-export function getStyle(element, property) {
-    let styleValue = window.getComputedStyle(document.getElementsByClassName(element)[0], null).getPropertyValue(property);
-    styleValue = parseInt(styleValue.substr(0, styleValue.length - 2));
-    return styleValue
+function getStyle(element, property) {
+  let styleValue = window.getComputedStyle(document.getElementsByClassName(element)[0], null)
+    .getPropertyValue(property);
+  styleValue = parseInt(styleValue.substr(0, styleValue.length - 2), 10);
+  return styleValue;
 }
-
+export default getStyle;
